1444d9dafa183a8f86721bcdaa6bede75e87f116
allan <allan.farinango@est.ikiam.edu.ec>
2022-02-22 00:31:00 -0500

    ALLAN FARINANGO realizo el ejercicio 2

--- E2_ExamenParcial_Python/miningscience.py
+++ E2_ExamenParcial_Python/miningscience.py
@@ -8,22 +8,14 @@ def download_pubmed(keyword):
     handle = Entrez.efetch(db="nucleotide", id="MW196737.1", rettype="gb", retmode="text")
     record=SeqIO.read(handle,'gb')
     handle.close()
+    return 
     
 
-def multic(lista, mult=3): #SE PREDEFINE =3 POR SI ACASO NO SE DEFINA EL PARAMETRO, EL 3 SERA EL PREDEFINIDO#
-    '''la funcion definida multiplica n veces el numero de elementos
-     el parametro esta predefino para mult=3 de un conjunto mediante la funcion y el for '''
-    #print(handle.readline().strip())
-    w = []
-    for i in range(len(lista)):
-        e = mult* len(lista[i])
-        w.append(e)
-    return w
-
-def enum_caracteres(lista):
-    '''la funcion  cuncion y el for '''
-    c = []
-    for i in range(len(lista)):
-        a = enumerate(lista[i])
-        c.append(a)
-    return c
+def mining_pubs(tipo):
+    """Docstring mining_pubs"""
+    if tipo == "DP": 
+    if tipo == "AU":
+    if tipo == "AD":
+        
+    
+    return 
