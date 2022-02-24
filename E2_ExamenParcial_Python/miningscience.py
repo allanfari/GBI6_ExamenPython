c3fd24cb3c67bdcf8ffe262ae28d6958d828dbbf
allan <allan.farinango@est.ikiam.edu.ec>
2022-02-23 23:13:34 -0500

    ALLAN FARINANGO realizo correcciones al grafico del ejercicio 6 mejoro la primera y segunda funcion del ejercicio 1

--- E2_ExamenParcial_Python/miningscience.py
+++ E2_ExamenParcial_Python/miningscience.py
@@ -1,13 +1,65 @@
+
 from Bio import Entrez
-from Bio import SeqIO
 ###############
 
 def download_pubmed(keyword): 
-    """descargar la data de NCBI por gb formato detallado de Ecuador Genomics"""
+    """cargar y buscar la data de NCBI de cualquier caracter al que se le defina como keyword"""
     Entrez.email = 'allan.farinango@est.ikiam.edu.ec'
     handle = Entrez.esearch(db = 'pubmed', term= keyword + "[title/abstract]", usehistory = 'y')
     record = Entrez.read(handle)
     id_list = record['IdList']
     print('El número de artículos es:')
     print(record['Count'])
+   
+
+
     
+def mining_pubs(tipo):
+    """
+    Carga el archivo de pubmed y se lee tipo texto y busca los titulos DP, AU, AD y ordena su informacion segun parametros independientes
+    """
+    with open("data/pubmed-sarampion-set.txt", errors="ignore") as f: 
+        texto = f.read() 
+    if tipo == "DP":
+        PMID = re.findall("PMID- (\d*)", texto) 
+        year = re.findall("DP\s{2}-\s(\d{4})", texto)
+        pmid_y = pd.DataFrame()
+        pmid_y["PMID"] = PMID
+        pmid_y["Año de publicación"] = year
+        return (pmid_y)
+    elif tipo == "AU": 
+        PMID = re.findall("PMID- (\d*)", texto) 
+        autores = texto.split("PMID- ")
+        autores.pop(0)
+        num_autores = []
+        for i in range(len(autores)):
+            numero = re.findall("AU -", autores[i])
+            n = (len(numero))
+            num_autores.append(n)
+        pmid_a = pd.DataFrame()
+        pmid_a["PMID"] = PMID 
+        pmid_a["Numero de autores"] = num_autores
+        return (pmid_a)
+    elif tipo == "AD": 
+        texto = re.sub(r" [A-Z]{1}\.","", texto)
+        texto = re.sub(r"Av\.","", texto)
+        texto = re.sub(r"Vic\.","", texto)
+        texto = re.sub(r"Tas\.","", texto)
+        AD = texto.split("AD  - ")
+        n_paises = []
+        for i in range(len(AD)): 
+            pais = re.findall("\S, ([A-Za-z]*)\.", AD[i])
+            if not pais == []: 
+                if not len(pais) >= 2:  
+                    if re.findall("^[A-Z]", pais[0]): 
+                        n_paises.append(pais[0])
+        conteo=Counter(n_paises)
+        resultado = {}
+        for clave in conteo:
+            valor = conteo[clave]
+            if valor != 1: 
+                resultado[clave] = valor 
+        veces_pais = pd.DataFrame()
+        veces_pais["pais"] = resultado.keys()
+        veces_pais["numero de autores"] = resultado.values()
+        return (veces_pais)
